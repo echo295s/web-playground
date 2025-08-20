ba482110dbea4c3026260b5d41469634296ac6d5
echo295s <fh10926287@gmail.com>
2025-08-20 13:03:07 +0900

    refactor: split app into components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,61 +1,12 @@
 <template>
-  <div v-if="!isLoggedIn">
-    <h2>ログイン</h2>
-  </div>
-  <div v-else>
-    <h2>ようこそ「{{ username }}」さん</h2>
-    <input name="newMessage" v-model="newMessage" placeholder="メッセージを入力" />
-    <button @click="sendMessage">送信</button>
-    <ul>
-      <li v-for="msg in messages" :key="msg.id">
-        {{ msg.content }} ({{ msg.created_at }})
-      </li>
-    </ul>
-  </div>
+  <Login v-if="!isLoggedIn" />
+  <Chat v-else />
 </template>
 
-<script>
-import { apiURL } from './config.js'
-import axios from 'axios'
+<script setup>
+import Chat from './components/Chat.vue'
+import Login from './components/Login.vue'
 
-export default {
-  data() {
-    return {
-      apiURL,
-      newMessage: '',
-      messages: [],
-      username: ''
-    }
-  },
-  computed: {
-    isLoggedIn() {
-      return true;
-    },
-  },
-  methods: {
-    async sendMessage() {
-      if (!this.newMessage) return
-      try {
-        await axios.post(`${this.apiURL}/message`, {
-          content: this.newMessage
-        })
-        this.newMessage = ''
-        await this.fetchMessages()
-      } catch (error) {
-        console.error('送信失敗', error)
-      }
-    },
-    async fetchMessages() {
-      try {
-        const res = await axios.get(`${this.apiURL}/messages`)
-        this.messages = res.data.messages
-      } catch (error) {
-        console.error('取得失敗', error)
-      }
-    }
-  },
-  mounted() {
-    this.fetchMessages()
-  }
-}
+const isLoggedIn = true
 </script>
+
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login-container">
+    <h2>ログイン</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+.login-container {
+  text-align: center;
+  margin-top: 2rem;
+}
+</style>
+
