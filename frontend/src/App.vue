e366c7a64c09bd1dacbeb0800ffe4619fff349a5
echo295s <fh10926287@gmail.com>
2025-08-20 17:44:45 +0900

    feat: add login and registration forms

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <Login v-if="!isLoggedIn" />
-  <Chat v-else />
+  <Chat v-if="isLoggedIn" />
+  <Login
+    v-else-if="!isRegisterMode"
+    @login-success="isLoggedIn = true"
+    @go-register="isRegisterMode = true"
+  />
+  <Register v-else @go-login="isRegisterMode = false" />
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import Chat from './components/Chat.vue'
 import Login from './components/Login.vue'
+import Register from './components/Register.vue'
 
-const isLoggedIn = true
+const isLoggedIn = ref(!!localStorage.getItem('token'))
+const isRegisterMode = ref(false)
 </script>
+
