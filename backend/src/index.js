2f9a9eb10ddafbcace3396882d653065600d9df8
echo295s <fh10926287@gmail.com>
2025-08-21 12:01:47 +0900

    コード整理

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,19 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const messageRoutes = require('./routes/messages')
-const authRoutes = require('./routes/auth')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const app = express()
-const port = 4000
+const messageRoutes = require('./routes/messages');
+const authRoutes = require('./routes/auth');
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use('/api', messageRoutes)
-app.use('/api/auth', authRoutes)
+const app = express();
+const port = 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api', messageRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
-})
+});
