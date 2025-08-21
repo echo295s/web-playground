0208560466cf38f31d96ea2570d5149b845ce8ff
echo295s <fh10926287@gmail.com>
2025-08-21 16:47:27 +0900

    feat(frontend): add Vuetify setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import { createVuetify } from 'vuetify'
+
+const vuetify = createVuetify()
+
+createApp(App).use(vuetify).mount('#app')
