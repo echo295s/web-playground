5f6474a3880765449ee4ec38c11f7104138025fa
echo295s <fh10926287@gmail.com>
2025-08-25 16:49:31 +0900

    チャット修正

--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -2,7 +2,21 @@
   <v-card class="mx-auto my-8" max-width="600">
     <v-card-title>ようこそ「{{ username }}」さん</v-card-title>
     <v-card-text>
-      <div class="d-flex gap-2 mb-1">
+      <v-list class="message-list mt-2" ref="messageList">
+        <v-list-item
+          v-for="msg in messages"
+          :key="msg.id"
+        >
+          <template #prepend>
+            <v-avatar size="24" class="user-icon">
+              <img :src="userIcon" alt="user icon" />
+            </v-avatar>
+          </template>
+          <v-list-item-title>{{ msg.content }}</v-list-item-title>
+          <v-list-item-subtitle>{{ msg.username || '名無しさん' }} ({{ msg.created_at }})</v-list-item-subtitle>
+        </v-list-item>
+      </v-list>
+      <div class="d-flex gap-2 mt-4">
         <v-text-field
           name="newMessage"
           v-model="newMessage"
@@ -14,20 +28,6 @@
         />
         <v-btn color="primary" @click="sendMessage">送信</v-btn>
       </div>
-      <v-list class="message-list" ref="messageList">
-        <v-list-item
-          v-for="msg in messages"
-          :key="msg.id"
-        >
-          <template #prepend>
-            <v-avatar size="24" class="user-icon">
-              <img :src="userIcon" alt="user icon" />
-            </v-avatar>
-          </template>
-          <v-list-item-title>{{ msg.content }}</v-list-item-title>
-          <v-list-item-subtitle>({{ msg.created_at }})</v-list-item-subtitle>
-        </v-list-item>
-      </v-list>
     </v-card-text>
   </v-card>
 </template>
