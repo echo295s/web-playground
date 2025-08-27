875b28be5ec37f12dcd31fbbc2caa0ec42bbfe5c
echo295s <fh10926287@gmail.com>
2025-08-27 14:36:12 +0900

    feat: add app selection screen and dummy apps

---
+++ frontend/src/components/DummyApp2.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card class="mx-auto my-12" max-width="400">
+    <v-card-title>ダミーアプリ2</v-card-title>
+    <v-card-text>ここはダミーアプリ2です。</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'DummyApp2',
+};
+</script>
+
